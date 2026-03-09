4b2047bd4147aa7a40809a8f60c189dbbc9f6ce3
unknown <charleskeyz2017@gmail.com>
2026-03-09 16:15:24 +0100

    commit code

--- src/app/contact-us/_components/ContactCards.tsx
+++ src/app/contact-us/_components/ContactCards.tsx
@@ -8,61 +8,62 @@ import { IoLocationOutline } from "react-icons/io5";
 import { RxEnvelopeClosed } from "react-icons/rx";
 
 const ContactCards = () => {
-	const { data: generalSettings, isLoading, isError } = useGeneralSettings();
+  const { data: generalSettings, isLoading, isError } = useGeneralSettings();
 
-	const GeneralSettings: WooCommerceSetting[] = generalSettings;
+  const GeneralSettings: WooCommerceSetting[] = generalSettings;
 
-	// console.log("GeneralSettings", GeneralSettings);
+  // console.log("GeneralSettings", GeneralSettings);
 
-	const contactCardData = [
-		{
-			id: 1,
-			title: "Email Us",
-			type: "email",
-			icon: <RxEnvelopeClosed className='text-primary text-2xl xl:text-4xl' />,
-			additionalText: GeneralSettings ? GeneralSettings[0]?.value : "N/A",
-		},
-		{
-			id: 2,
-			title: "Call Us",
-			icon: <FiPhoneCall className='text-primary text-2xl xl:text-4xl' />,
-			type: "tel",
-			additionalText: GeneralSettings ? GeneralSettings[1]?.value : "N/A",
-		},
-		{
-			id: 3,
-			title: "Location",
-			type: "text",
-			icon: <IoLocationOutline className='text-primary text-2xl xl:text-4xl' />,
-			description: GeneralSettings ? GeneralSettings[2]?.value : "N/A",
-		},
-		// Add more contact card data here if needed
-	];
-	return (
-		<>
-			{generalSettings &&
-				contactCardData?.map((card) => (
-					<ContactCard
-						key={card.id}
-						isLoading={isLoading}
-						type={card.type}
-						title={card.title}
-						icon={card.icon}
-						additionalText={card.additionalText}
-						// additionalText2={card.additionalText2}
-						description={card.description}
-					/>
-				))}
+  const contactCardData = [
+    {
+      id: 1,
+      title: "Email Us",
+      type: "email",
+      icon: <RxEnvelopeClosed className="text-primary text-2xl xl:text-4xl" />,
+      additionalText: "layooke03@gmail.com",
+    },
+    {
+      id: 2,
+      title: "Call Us",
+      icon: <FiPhoneCall className="text-primary text-2xl xl:text-4xl" />,
+      type: "tel",
+      additionalText: "8105753207",
+    },
+    {
+      id: 3,
+      title: "Location",
+      type: "text",
+      icon: <IoLocationOutline className="text-primary text-2xl xl:text-4xl" />,
+      additionalText: "ADEOYE IFEDAPO EKEMINIABASI",
+      description: "8, SHIKIRI STREET, , IKORODU, LAGOS STATE, NIGERIA",
+    },
+    // Add more contact card data here if needed
+  ];
+  return (
+    <>
+      {!isLoading &&
+        contactCardData?.map((card) => (
+          <ContactCard
+            key={card.id}
+            isLoading={isLoading}
+            type={card.type}
+            title={card.title}
+            icon={card.icon}
+            additionalText={card.additionalText}
+            // additionalText2={card.additionalText2}
+            description={card.description}
+          />
+        ))}
 
-			{isLoading && (
-				<>
-					<Skeleton className='flex flex-col w-[15rem] bg-slate-200 slg:w-[18rem] xl:w-[23rem] shadow-md rounded-md py-5 sm:py-20 xl:py-32 items-center justify-center text-center px-4 xl:px-1 animate-pulse' />
-					<Skeleton className='flex flex-col w-[15rem] bg-slate-200 slg:w-[18rem] xl:w-[23rem] shadow-md rounded-md py-5 sm:py-20 xl:py-32 items-center justify-center text-center px-4 xl:px-1 animate-pulse' />
-					{/* <Skeleton className='flex flex-col w-[15rem] bg-slate-200 slg:w-[18rem] xl:w-[23rem] shadow-md rounded-md py-5 sm:py-20 xl:py-32 items-center justify-center text-center px-4 xl:px-1 animate-pulse' /> */}
-				</>
-			)}
-		</>
-	);
+      {isLoading && (
+        <>
+          <Skeleton className="flex flex-col w-[15rem] bg-slate-200 slg:w-[18rem] xl:w-[23rem] shadow-md rounded-md py-5 sm:py-20 xl:py-32 items-center justify-center text-center px-4 xl:px-1 animate-pulse" />
+          <Skeleton className="flex flex-col w-[15rem] bg-slate-200 slg:w-[18rem] xl:w-[23rem] shadow-md rounded-md py-5 sm:py-20 xl:py-32 items-center justify-center text-center px-4 xl:px-1 animate-pulse" />
+          {/* <Skeleton className='flex flex-col w-[15rem] bg-slate-200 slg:w-[18rem] xl:w-[23rem] shadow-md rounded-md py-5 sm:py-20 xl:py-32 items-center justify-center text-center px-4 xl:px-1 animate-pulse' /> */}
+        </>
+      )}
+    </>
+  );
 };
 
 export default ContactCards;
